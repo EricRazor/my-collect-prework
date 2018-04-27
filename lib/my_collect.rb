eaf37312a1e4b25bdf6f1b21dba23a6155793d5c
Eric Razor <eric.razorjr@gmail.com>
2018-04-27 05:22:01 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,7 @@
 def my_collect(array)
   new_array = [] 
   while length < array.length do
-    new_array << yield array[length]
-    
+    new_array << yield array[length].upcase
+    new_array.split("")
   end
-  
 end
